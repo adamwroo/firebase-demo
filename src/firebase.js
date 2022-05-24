3137e268960a0c859495889ac3b2509fdb07a137
adam.wroblewski <adam.wroblewski@unic.com>
2022-05-24 15:00:36 +0200

    firebase installed and configured

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB3cvp0xO8Fe6h5Mo6vTwVTWD2Gl-uYuoQ",
+  authDomain: "fir-demo-rti.firebaseapp.com",
+  databaseURL: "https://fir-demo-rti-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "fir-demo-rti",
+  storageBucket: "fir-demo-rti.appspot.com",
+  messagingSenderId: "668457948993",
+  appId: "1:668457948993:web:a9dad6cc180fc850c773c0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
